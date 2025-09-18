a9596f54892d7fb683c54c16f93142045c4ffb35
pmr-fiz <andriano030706@gmail.com>
2025-09-18 17:31:07 +0300

    Add files via upload

---
+++ Laboratory 1/Palindrome.java
@@ -0,0 +1,25 @@
+public class Palindrome {
+    public static void main(String[] args) {
+        for (int i = 0; i < args.length; i++) {
+            String s = args[i];
+            if (isPalindrome(s)) {
+                System.out.println(s + " is palindrome");
+            } else {
+                System.out.println(s + " is not palindrome");
+            }
+        }
+    }
+
+    public static String reverseString(String s) {
+        String reversed = "";
+        for (int i = s.length() - 1; i >= 0; i--) {
+            reversed = reversed + s.charAt(i);
+        }
+        return reversed;
+    }
+
+    public static boolean isPalindrome(String s) {
+        String reversed = reverseString(s);
+        return s.equals(reversed);
+    }
+}
